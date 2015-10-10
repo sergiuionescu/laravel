28b6de4d1d312edda1dbe775cf9ec6c3b5669807
Sergiu Ionescu <sergiu.ionescu@gmail.com>
2015-10-10 17:41:39 +0300

    Updating the version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sergiu.ionescu@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures laravel'
 long_description 'Installs/Configures laravel'
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'php'
 depends 'lamp'
