979e924f1dae26d3b3f29b43436915d30a1501a1
Sergiu Ionescu <sergiu.ionescu@gmail.com>
2016-03-12 17:48:07 +0200

    Adding support metadata

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,7 @@ license          'Apache 2.0'
 description      'Installs/Configures laravel'
 long_description 'Installs/Configures laravel'
 version          '0.1.2'
+source_url       'https://github.com/sergiuionescu/laravel' if respond_to?(:source_url)
+issues_url       'https://github.com/sergiuionescu/lamp/laravel' if respond_to?(:issues_url)
 
 depends 'lamp'
