18b33d1f1086d00c9f66b7b3949824b85e8f009b
Sergiu Ionescu <sergiu.ionescu@gmail.com>
2016-03-13 19:38:20 +0200

    Fixed issues url

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Installs/Configures laravel'
 long_description 'Installs/Configures laravel'
 version          '0.1.2'
 source_url       'https://github.com/sergiuionescu/laravel' if respond_to?(:source_url)
-issues_url       'https://github.com/sergiuionescu/lamp/laravel' if respond_to?(:issues_url)
+issues_url       'https://github.com/sergiuionescu/laravel/issues' if respond_to?(:issues_url)
 
 depends 'lamp'
